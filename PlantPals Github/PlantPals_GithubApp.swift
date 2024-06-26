2e0d1452ece93876ba1176330490bf1046a1ee74
Gavin Wagner <gavin6078@gmail.com>
2024-06-25 17:28:08 -1000

    Initial Commit

---
+++ PlantPals Github/PlantPals_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantPals_GithubApp.swift
+//  PlantPals Github
+//
+//  Created by Gavin Wagner on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PlantPals_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
